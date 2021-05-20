916867479fe551a95ed42e56a9425124cefb1df6
Tom Crijns <1816802crijns@zuyd.nl>
2021-05-20 20:10:38 +0200

    Feature for users to login to portal and style that does fit better with the company

---
+++ RuilWinkelVaals/RuilWinkelVaals/ViewModel/Login.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RuilWinkelVaals.ViewModel
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "Er is geen e-mailadres ingevuld")]
+        [Display(Name = "E-mailadres")]
+        public string emailAddress { get; set; }
+        [Required(ErrorMessage = "Er is geen wachtwoord ingevuld")]
+        [Display(Name = "Wachtwoord")]
+        public string password { get; set; }
+        public int loginAttempts { get; set; }
+    }
+}
