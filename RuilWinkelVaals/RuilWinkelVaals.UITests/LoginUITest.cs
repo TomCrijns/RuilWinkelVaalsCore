94eaf1687bcc7da1a2258ed30a408170f47a4ece
Tom Crijns <1816802crijns@zuyd.nl>
2021-05-24 14:06:15 +0200

    Some Id's with space changed because of testing failure. Id's will be fixed in next release.

--- RuilWinkelVaals/RuilWinkelVaals.UITests/LoginUITest.cs
+++ RuilWinkelVaals/RuilWinkelVaals.UITests/LoginUITest.cs
@@ -14,13 +14,13 @@ namespace RuilWinkelVaals.UITests
         public void Login()
         {
             //Arange
-            string url = "https://test-ruilwinkelvaals.azurewebsites.net/Login/Login";
+            string url = "https://test-ruilwinkelvaalscore.azurewebsites.net/Login/Login";
             ChromeDriver driver = new ChromeDriver(@"D:\School\Vakken\B2C6\TestProject1\TestProject1\bin\Debug\net5.0");
             
             //Act
             driver.Navigate().GoToUrl(url);
             driver.Manage().Window.Maximize();
-            driver.FindElement(By.Id("EmailTextBox")).SendKeys("testuser1@ruilwinkelvaals.nl");
+            driver.FindElement(By.ClassName("Textbox")).SendKeys("testuser1@ruilwinkelvaals.nl");
             driver.FindElement(By.Id("PasswordTextBox")).SendKeys("test2021");
             driver.FindElement(By.ClassName("Button")).Click();
             WebDriverWait wait = new WebDriverWait(driver, new System.TimeSpan(0, 0, 15));
@@ -36,111 +36,111 @@ namespace RuilWinkelVaals.UITests
         public void IncorrectEmail()
         {
             //Arange
-            string url = "https://test-ruilwinkelvaals.azurewebsites.net/Login/Login";
+            string url = "https://test-ruilwinkelvaalscore.azurewebsites.net/Login/Login";
             ChromeDriver driver = new ChromeDriver(@"D:\School\Vakken\B2C6\TestProject1\TestProject1\bin\Debug\net5.0");
 
             //Act
             driver.Navigate().GoToUrl(url);
             driver.Manage().Window.Maximize();
-            driver.FindElement(By.Id("EmailTextBox")).SendKeys("testuser@ruilwinkelvaals.nl");
+            driver.FindElement(By.ClassName("Textbox")).SendKeys("testuser@ruilwinkelvaals.nl");
             driver.FindElement(By.Id("PasswordTextBox")).SendKeys("test2021");
             driver.FindElement(By.ClassName("Button")).Click();
-            WebDriverWait wait = new WebDriverWait(driver, new System.TimeSpan(0, 0, 15));
+            WebDriverWait wait = new WebDriverWait(driver, new System.TimeSpan(0, 0, 20));
             wait.Until(wt => wt.FindElement(By.Id("LoginValidationError")));
 
             var text = driver.FindElement(By.Id("LoginValidationError"));
 
             //Assert
-            Assert.IsTrue(text.Text.Contains("De gebruikersnaam/wachtwoord is incorrect"));
+            Assert.IsTrue(text.Text.Contains("DE GEBRUIKERSNAAM/WACHTWOORD IS INCORRECT"));
         }
 
         [TestMethod]
         public void IncorrectPassword()
         {
             //Arange
-            string url = "https://test-ruilwinkelvaals.azurewebsites.net/Login/Login";
+            string url = "https://test-ruilwinkelvaalscore.azurewebsites.net/Login/Login";
             ChromeDriver driver = new ChromeDriver(@"D:\School\Vakken\B2C6\TestProject1\TestProject1\bin\Debug\net5.0");
 
             //Act
             driver.Navigate().GoToUrl(url);
             driver.Manage().Window.Maximize();
-            driver.FindElement(By.Id("EmailTextBox")).SendKeys("testuser1@ruilwinkelvaals.nl");
+            driver.FindElement(By.ClassName("Textbox")).SendKeys("testuser1@ruilwinkelvaals.nl");
             driver.FindElement(By.Id("PasswordTextBox")).SendKeys("test2021!");
             driver.FindElement(By.ClassName("Button")).Click();
-            WebDriverWait wait = new WebDriverWait(driver, new System.TimeSpan(0, 0, 15));
+            WebDriverWait wait = new WebDriverWait(driver, new System.TimeSpan(0, 0, 20));
             wait.Until(wt => wt.FindElement(By.Id("LoginValidationError")));
 
             var text = driver.FindElement(By.Id("LoginValidationError"));
 
             //Assert
-            Assert.IsTrue(text.Text.Contains("De gebruikersnaam/wachtwoord is incorrect"));
+            Assert.IsTrue(text.Text.Contains("DE GEBRUIKERSNAAM/WACHTWOORD IS INCORRECT"));
         }
 
         [TestMethod]
         public void NoEmailFilledIn()
         {
             //Arange
-            string url = "https://test-ruilwinkelvaals.azurewebsites.net/Login/Login";
+            string url = "https://test-ruilwinkelvaalscore.azurewebsites.net/Login/Login";
             ChromeDriver driver = new ChromeDriver(@"D:\School\Vakken\B2C6\TestProject1\TestProject1\bin\Debug\net5.0");
 
             //Act
             driver.Navigate().GoToUrl(url);
             driver.Manage().Window.Maximize();
-            driver.FindElement(By.Id("EmailTextBox")).SendKeys("");
+            driver.FindElement(By.ClassName("Textbox")).SendKeys("");
             driver.FindElement(By.Id("PasswordTextBox")).SendKeys("test2021");
             driver.FindElement(By.ClassName("Button")).Click();
-            WebDriverWait wait = new WebDriverWait(driver, new System.TimeSpan(0, 0, 15));
-            wait.Until(wt => wt.FindElement(By.Id("EmailValidation")));
+            WebDriverWait wait = new WebDriverWait(driver, new System.TimeSpan(0, 0, 20));
+            wait.Until(wt => wt.FindElement(By.ClassName("Validation")));
 
-            var text = driver.FindElement(By.Id("EmailValidation"));
+            var text = driver.FindElement(By.ClassName("Validation"));
 
             //Assert
-            Assert.IsTrue(text.Text.Contains("Er is geen e-mailadres ingevuld"));
+            Assert.IsTrue(text.Text.Contains("ER IS GEEN E-MAILADRES INGEVULD"));
         }
 
         [TestMethod]
         public void NoPasswordFilledIn()
         {
             //Arange
-            string url = "https://test-ruilwinkelvaals.azurewebsites.net/Login/Login";
+            string url = "https://test-ruilwinkelvaalscore.azurewebsites.net/Login/Login";
             ChromeDriver driver = new ChromeDriver(@"D:\School\Vakken\B2C6\TestProject1\TestProject1\bin\Debug\net5.0");
 
             //Act
             driver.Navigate().GoToUrl(url);
             driver.Manage().Window.Maximize();
-            driver.FindElement(By.Id("EmailTextBox")).SendKeys("testuser1@ruilwinkelvaals.nl");
+            driver.FindElement(By.ClassName("Textbox")).SendKeys("testuser1@ruilwinkelvaals.nl");
             driver.FindElement(By.Id("PasswordTextBox")).SendKeys("");
             driver.FindElement(By.ClassName("Button")).Click();
-            WebDriverWait wait = new WebDriverWait(driver, new System.TimeSpan(0, 0, 15));
+            WebDriverWait wait = new WebDriverWait(driver, new System.TimeSpan(0, 0, 20));
             wait.Until(wt => wt.FindElement(By.Id("PasswordValidation")));
 
             var text = driver.FindElement(By.Id("PasswordValidation"));
 
             //Assert
-            Assert.IsTrue(text.Text.Contains("Er is geen wachtwoord ingevuld"));
+            Assert.IsTrue(text.Text.Contains("ER IS GEEN WACHTWOORD INGEVULD"));
         }
         [TestMethod]
         public void NoEmailAndPasswordFilledIn()
         {
             //Arange
-            string url = "https://test-ruilwinkelvaals.azurewebsites.net/Login/Login";
+            string url = "https://test-ruilwinkelvaalscore.azurewebsites.net/Login/Login";
             ChromeDriver driver = new ChromeDriver(@"D:\School\Vakken\B2C6\TestProject1\TestProject1\bin\Debug\net5.0");
 
             //Act
             driver.Navigate().GoToUrl(url);
             driver.Manage().Window.Maximize();
-            driver.FindElement(By.Id("EmailTextBox")).SendKeys("");
+            driver.FindElement(By.ClassName("Textbox")).SendKeys("");
             driver.FindElement(By.Id("PasswordTextBox")).SendKeys("");
             driver.FindElement(By.ClassName("Button")).Click();
-            WebDriverWait wait = new WebDriverWait(driver, new System.TimeSpan(0, 0, 15));
+            WebDriverWait wait = new WebDriverWait(driver, new System.TimeSpan(0, 0, 20));
             wait.Until(wt => wt.FindElement(By.Id("PasswordValidation")));
 
-            var emailValidation = driver.FindElement(By.Id("EmailValidation"));
+            var emailValidation = driver.FindElement(By.ClassName("Validation"));
             var passwordValidation = driver.FindElement(By.Id("PasswordValidation"));
 
             //Assert
-            Assert.IsTrue(passwordValidation.Text.Contains("Er is geen wachtwoord ingevuld"));
-            Assert.IsTrue(emailValidation.Text.Contains("Er is geen e-mailadres ingevuld"));
+            Assert.IsTrue(passwordValidation.Text.Contains("ER IS GEEN WACHTWOORD INGEVULD"));
+            Assert.IsTrue(emailValidation.Text.Contains("ER IS GEEN E-MAILADRES INGEVULD"));
         }
     }
 }
