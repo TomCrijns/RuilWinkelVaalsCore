9d54a0e3ca4d2bf5eedbac9f090205bc7f1191c1
Jek <jekkowdev@gmail.com>
2021-06-01 13:56:22 +0200

    Added BlockedAccount after 3 failed attemps with wrong credentials

--- RuilWinkelVaals/RuilWinkelVaals/Controllers/LoginController.cs
+++ RuilWinkelVaals/RuilWinkelVaals/Controllers/LoginController.cs
@@ -25,22 +25,61 @@ namespace RuilWinkelVaals.Controllers
             if (ModelState.IsValid)
             {
                 bool authenticated = false;
+                AccountDatum userCredentials = new AccountDatum();
                 var profileData = db.ProfileData.Where(user => user.Email == model.emailAddress).FirstOrDefault();
                 if (profileData != null)
                 {
-                    var userCredentials = db.AccountData.Where(user => user.ProfileId == profileData.Id).FirstOrDefault();
+                    userCredentials = db.AccountData.Where(user => user.ProfileId == profileData.Id).FirstOrDefault();
                     authenticated = Authentication.AuthenticateUser(model.emailAddress, profileData.Email, model.password, userCredentials.Hash, userCredentials.Salt);
                 }
                 if (authenticated)
                 {
-                    HttpContext.Session.SetInt32("UserId", profileData.Id);
-                    return RedirectToAction("Privacy", "Home");
+                    if(userCredentials.Blocked == true)
+                    {
+                        //UserAccount Blocked status is true, access is denied
+                        ModelState.AddModelError("LoginError", "Uw account is geblokkeerd, u heeft teveel foutieve pogingen geprobeerd. Neem contact op met de klantenservice van RuilWinkel Vaals.");
+                        return View();
+                    }
+                    else
+                    {
+                        //UserAccount Blocked status is false, access is granted
+                        HttpContext.Session.SetInt32("UserId", profileData.Id);
+                        return RedirectToAction("Privacy", "Home");
+                    }
                 }
                 else
                 {
-                    ModelState.AddModelError("LoginError", "De gebruikersnaam/wachtwoord is incorrect");
+                    //If the given Mailadres is not equal to the previous mailadres
+                    if(HttpContext.Session.GetString("MailAdres") != model.emailAddress)
+                    {
+                        HttpContext.Session.SetString("MailAdres", model.emailAddress);
+                        HttpContext.Session.SetInt32("BlockedCounter", 1);
+                        ModelState.AddModelError("LoginError", "De gebruikersnaam/wachtwoord is incorrect");
+                        return View();
+                    }
+                    //Current Mail adres and previous mail adres are equal
 
-                    return View();
+                    //Add 1 to the BlockedValue
+                    int BlockedValue = Convert.ToInt32(HttpContext.Session.GetInt32("BlockedCounter")) + 1;
+                    HttpContext.Session.SetInt32("BlockedCounter", BlockedValue);
+
+                    //Check if BlockedValue >= 3, User will get blocked
+                    if(BlockedValue >= 3)
+                    {
+                        //AccountData change to blocked and saveChanges
+                        userCredentials.Blocked = true;
+                        userCredentials.DateBlocked = DateTime.Now.Date;
+                        db.AccountData.Update(userCredentials);
+                        db.SaveChanges();
+
+                        ModelState.AddModelError("LoginError", "Uw account is geblokkeerd, u heeft teveel foutieve pogingen geprobeerd. Neem contact op met de klantenservice van RuilWinkel Vaals.");
+                        return View();
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("LoginError", "De gebruikersnaam/wachtwoord is incorrect");
+                        return View();
+                    }
                 }
             }
             else
