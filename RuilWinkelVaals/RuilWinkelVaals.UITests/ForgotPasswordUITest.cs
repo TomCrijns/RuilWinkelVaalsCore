5645c4f0f3f18ca994c3aca2cd5b7bd1de35c7eb
Tom Crijns <1816802crijns@zuyd.nl>
2021-06-01 19:41:21 +0200

    Added class of password forgotten testing

---
+++ RuilWinkelVaals/RuilWinkelVaals.UITests/ForgotPasswordUITest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace RuilWinkelVaals.UITests
+{
+    [TestClass]
+    public class ForgotPasswordUITest
+    {
+        [TestMethod]
+        public void Test()
+        {
+            //Arange
+            string url = "https://www.google.com";
+            ChromeDriver driver = new ChromeDriver(@"D:\School\Vakken\B2C6\TestProject1\TestProject1\bin\Debug\net5.0");
+
+            //Act
+            driver.Navigate().GoToUrl(url);
+        }
+    }
+}
