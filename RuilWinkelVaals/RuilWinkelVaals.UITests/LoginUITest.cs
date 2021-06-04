c1c09980b8ff8c3bf159a6931c46b9ee350ee552
Tom Crijns <1816802crijns@zuyd.nl>
2021-06-04 09:39:55 +0200

    Added ID to check if automatic testing for registration functions succeeds

--- RuilWinkelVaals/RuilWinkelVaals.UITests/LoginUITest.cs
+++ RuilWinkelVaals/RuilWinkelVaals.UITests/LoginUITest.cs
@@ -202,9 +202,10 @@ namespace RuilWinkelVaals.UITests
                 driver.FindElement(By.Id("registerButton")).Click();
                 WebDriverWait wait = new WebDriverWait(driver, new System.TimeSpan(0, 1, 0));
 
-                wait.Until(wt => wt.FindElement(By.ClassName("Validation")));
-                var message = driver.FindElement(By.ClassName("Validation"));
-                Assert.IsTrue(message.Text.Contains("Er is geen e-mailadres ingevuld"));
+                wait.Until(wt => wt.FindElement(By.Id("EmailValidation")));
+                var message = driver.FindElement(By.Id("EmailValidation"));
+                Assert.AreEqual("Er is geen e-mailadres ingevuld", message.Text);
+                //Assert.IsTrue(message.Text.Contains("Er is geen e-mailadres ingevuld"));
             }
         }
     }
